4daca7ab5b17cda5d9aeea94e465edf59600fcb2
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-15 19:22:47 -0300

    references and borrowing

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,63 @@ fn main() {
 
         println!("The length of '{}' is {}.", s2, len);
     }
+    {
+        let s1 = String::from("hello");
+
+        let len = calculate_length_ref(&s1);
+
+        println!("The length of '{}' is {}.", s1, len);
+    }
+    {
+        let mut s = String::from("hello");
+
+        change(&mut s);
+        println!("{}", s);
+    }
+    {
+        let mut s = String::from("hello");
+
+        let r1 = &mut s;
+        // let r2 = &mut s; This fail only one mutable reference is valid
+        //error[E0499]: cannot borrow `s` as mutable more than once at a time
+
+        println!("{}", r1);
+    }
+
+    {
+        let mut s = String::from("hello");
+
+        {
+            let r1 = &mut s;
+            println!("{}", r1);
+        } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+        let r2 = &mut s;
+        println!("{}", r2);
+    }
+
+    {
+        let mut s = String::from("hello");
+
+        let r1 = &s; // no problem
+        let r2 = &s; // no problem
+                     // let r3 = &mut s; BIG PROBLEM
+                     //error[E0502]: cannot borrow `s` as mutable because it is also borrowed as immutable
+
+        println!("{}, {}, {}", s, r1, r2);
+    }
+
+    {
+        let mut s = String::from("hello");
+
+        let r1 = &s; // no problem
+        let r2 = &s; // no problem
+        println!("{} and {}", r1, r2);
+        // variables r1 and r2 will not be used after this point
+
+        let r3 = &mut s; // no problem
+        println!("{}", r3);
+    }
 }
 
 fn takes_ownership(some_string: String) {
@@ -98,4 +155,14 @@ fn calculate_length(s: String) -> (String, usize) {
     let length = s.len(); // len() returns the length of a String
 
     (s, length)
+}
+
+fn calculate_length_ref(s: &String) -> usize {
+    // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
