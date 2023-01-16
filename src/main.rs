0016a1ef2e3d524fe7e17960259f855ea6b76545
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-16 08:09:29 -0300

    the slice type

--- src/main.rs
+++ src/main.rs
@@ -118,6 +118,75 @@ fn main() {
         let r3 = &mut s; // no problem
         println!("{}", r3);
     }
+
+    // SLICE TYPE
+    {
+        let mut s = String::from("hello world");
+
+        let word = first_word(&s);
+
+        // s.clear();  error!
+
+        println!("the first word is: {}", word);
+    }
+    {
+        let s = String::from("hello");
+
+        let slice = &s[0..2];
+        println!("{}", slice);
+        let slice = &s[..2];
+        println!("{}", slice);
+    }
+    {
+        let s = String::from("hello");
+
+        let len = s.len();
+
+        let slice = &s[3..len];
+        println!("{}", slice);
+
+        let slice = &s[3..];
+        println!("{}", slice);
+    }
+    {
+        let s = String::from("hello");
+
+        let len = s.len();
+
+        let slice = &s[0..len];
+        println!("{}", slice);
+
+        let slice = &s[..];
+        println!("{}", slice);
+    }
+
+    {
+        let my_string = String::from("hello world");
+
+        // `first_word` works on slices of `String`s, whether partial or whole
+        let word = first_word(&my_string[0..6]);
+        let word = first_word(&my_string[..]);
+        // `first_word` also works on references to `String`s, which are equivalent
+        // to whole slices of `String`s
+        let word = first_word(&my_string);
+
+        let my_string_literal = "hello world";
+
+        // `first_word` works on slices of string literals, whether partial or whole
+        let word = first_word(&my_string_literal[0..6]);
+        let word = first_word(&my_string_literal[..]);
+
+        // Because string literals *are* string slices already,
+        // this works too, without the slice syntax!
+        let word = first_word(my_string_literal);
+        println!("{}", word);
+    }
+    {
+        let a = [1, 2, 3, 4, 5];
+
+        let slice = &a[1..3];
+        println!("{}", slice[0]);
+    }
 }
 
 fn takes_ownership(some_string: String) {
@@ -165,4 +234,16 @@ fn calculate_length_ref(s: &String) -> usize {
 
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
